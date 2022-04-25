7121cdee69f81c59025151f119c20a129b1d9237
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2022-04-24 19:12:48 -0500

    android project

---
+++ app/src/main/java/edu/eci/ieti/ieti_first_android_proyect/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.eci.ieti.ieti_first_android_proyect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/edu/eci/ieti/ieti_first_android_proyect/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package edu.eci.ieti.ieti_first_android_proyect
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
